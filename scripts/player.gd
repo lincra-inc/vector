7976aec2242438eece1fb75a8b82c24485f8ce62
thomm23 <tomygullari1999@gmail.com>
2026-07-25 17:43:40 -0300

    MOVIMIENTO
    apretas la r y te reiniciar la vida

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody3D
 class_name Player
 
-const SPEED: float = 6.0
 const MOUSE_SENS: float = 0.003
-const GRAVITY: float = 20.0
+
+const MAX_SPEED := 10.0
+const ACCEL := 70.0
+const AIR_ACCEL := 35.0
+const FRICTION := 10.0
+
+const JUMP_FORCE := 9.0
+
+const GRAVITY := 18.0
+const FALL_GRAVITY := 45.
 
 @onready var camera: Camera3D = $Head/Camera3D
 @onready var input_state: PlayerInputState = $InputState
@@ -25,6 +33,7 @@ var view_rotation := Vector2.ZERO
 
 var pitch := 0.0
 var yaw := 0.0
+var spawn_position: Vector3
 
 func _enter_tree():
 	set_multiplayer_authority(int(name))
@@ -36,6 +45,7 @@ func _ready():
 	if is_multiplayer_authority():
 		camera.current = true
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	spawn_position = global_position
 
 func _process(delta):
 	if multiplayer.get_unique_id() != int(name):
@@ -76,21 +86,78 @@ func _physics_process(delta):
 	move_player(delta)
 
 func move_player(delta):
+
 	if !is_multiplayer_authority():
 		return
-	
+
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+	if Input.is_action_just_pressed("Reiniciar"):
+		global_position = spawn_position
+		velocity = Vector3.ZERO
+
 	var input := Input.get_vector(
 		"ui_left",
 		"ui_right",
 		"ui_up",
 		"ui_down"
 	)
+
+	var wish_dir := (transform.basis * Vector3(input.x, 0, input.y)).normalized()
+
+	if is_on_floor():
+
+		apply_friction(delta)
+
+		accelerate(wish_dir, MAX_SPEED, ACCEL, delta)
+
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = JUMP_FORCE
+
+	else:
+
+		accelerate(wish_dir, MAX_SPEED, AIR_ACCEL, delta)
+
+		if velocity.y > 0:
+			velocity.y -= GRAVITY * delta
+		else:
+			velocity.y -= FALL_GRAVITY * delta
+
+	move_and_slide()
 	
-	var dir := (transform.basis.x * input.x - -transform.basis.z * input.y).normalized()
-	
-	velocity.x = dir.x * SPEED
-	velocity.z = dir.z * SPEED
-	
+func accelerate(wish_dir: Vector3, wish_speed: float, accel: float, delta: float):
+
+	var current_speed = velocity.dot(wish_dir)
+	var add_speed = wish_speed - current_speed
+
+	if add_speed <= 0:
+		return
+
+	var accel_speed = accel * delta * wish_speed
+
+	if accel_speed > add_speed:
+		accel_speed = add_speed
+
+	velocity += wish_dir * accel_speed
+
+
+func apply_friction(delta):
+
+	var horizontal = Vector3(velocity.x,0,velocity.z)
+
+	var speed = horizontal.length()
+
+	if speed < 0.01:
+		return
+
+	var drop = speed * FRICTION * delta
+	var new_speed = max(speed - drop,0.0)
+
+	horizontal *= new_speed / speed
+
+	velocity.x = horizontal.x
+	velocity.z = horizontal.z
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
 	
