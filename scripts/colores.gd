7eeb7cfb35ff4f740512fcae46239e57b82c402f
francoponce-externo-max <francoponce-externo-max@users.noreply.github.com>
2026-08-07 18:34:53 -0300

    colores de personajes

---
+++ scripts/colores.gd
@@ -0,0 +1,13 @@
+extends Node
+
+color = Color(1.353, 0.0, 0.033, 1.0)
+color = Color(1.353, 0.0, 1.175, 1.0)
+color = Color(0.619, 0.0, 1.353, 1.0)
+color = Color(0.0, 0.107, 1.353, 1.0)
+color = Color(0.0, 0.708, 1.353, 1.0)
+color = Color(0.0, 1.286, 1.353, 1.0)
+color = Color(0.0, 1.353, 0.508, 1.0)
+color = Color(0.283, 1.155, 0.0, 1.0)
+color = Color(0.752, 1.353, 0.0, 1.0)
+color = Color(1.353, 1.264, 0.0, 1.0)
+color = Color(1.353, 0.819, 0.0, 1.0)
